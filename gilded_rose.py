2a85f28a9ee32db39d41db78ace35647dbaa19d8
danny_huang <danny_huang@openfind.com.tw>
2025-05-05 15:20:33 +0800

    refactor: step 1 - remove duplicated condition of item Sulfuras

--- gilded_rose.py
+++ gilded_rose.py
@@ -8,15 +8,17 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
+            # Sulfuras, Hand of Ragnaros 是 Special Case, 略過
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                continue
+
             # 不需要在乎 sell_in 和 quality 的關係的物品的更新邏輯
             if (
                 item.name != "Aged Brie"
                 and item.name != "Backstage passes to a TAFKAL80ETC concert"
             ):
                 if item.quality > 0:
-                    # Sulfuras, Hand of Ragnaros 是 Special Case, 略過
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                    item.quality = item.quality - 1
             # sell_in 會影響 quality 更新邏輯的物品的更新邏輯 (Aged Brie 和 Backstage passes to a TAFKAL80ETC concert)
             else:
                 if item.quality < 50:
@@ -29,18 +31,14 @@ class GildedRose(object):
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-            # Sulfuras, Hand of Ragnaros 是 Special Case, 略過
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+            item.sell_in = item.sell_in - 1
             # 過期後的各類物品的品質更新邏輯
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
                         # 一般物品的過期後品質更新邏輯
                         if item.quality > 0:
-                            # Sulfuras, Hand of Ragnaros 是 Special Case, 略過
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                            item.quality = item.quality - 1
                     # Backstage passes to a TAFKAL80ETC concert 的過期後品質更新邏輯
                     else:
                         item.quality = item.quality - item.quality
