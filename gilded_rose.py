c9fa218381c6ffaaefb4099ab44500d6f36ec007
danny_huang <danny_huang@openfind.com.tw>
2025-05-06 11:10:35 +0800

    refactor: step 4 - seperate each item's quality update algorithm to seperated function

--- gilded_rose.py
+++ gilded_rose.py
@@ -12,43 +12,58 @@ class GildedRose(object):
 
         item.sell_in = item.sell_in - 1
 
+    def _clamp_quality(self, quality):
+        # 限制 quality 的範圍
+        return 50 if quality > 50 else 0 if quality < 0 else quality
+
+    def update_sulfuras_quality(self, item):
+        # Sulfuras, Hand of Ragnaros 是 Special Case, 略過
+        return
+
+    def update_aged_brie_quality(self, item):
+        # Aged Brie 的過期後品質更新邏輯
+        if item.sell_in < 0:
+            item.quality = item.quality + 2
+        # Aged Brie 的品質更新邏輯
+        else:
+            item.quality = item.quality + 1
+
+        item.quality = self._clamp_quality(item.quality)
+
+    def update_backstage_pass_quality(self, item):
+        # Backstage passes to a TAFKAL80ETC concert 的品質更新邏輯
+        if item.sell_in >= 10:
+            item.quality = item.quality + 1
+        elif item.sell_in < 10 and item.sell_in >= 5:
+            item.quality = item.quality + 2
+        elif item.sell_in < 5 and item.sell_in >= 0:
+            item.quality = item.quality + 3
+        # Backstage passes to a TAFKAL80ETC concert 的過期後品質更新邏輯
+        else:
+            item.quality = item.quality - item.quality
+
+        item.quality = self._clamp_quality(item.quality)
+
+    def udpate_normal_quality(self, item):
+        # 一般物品的過期後品質更新邏輯
+        if item.sell_in < 0:
+            item.quality = item.quality - 2
+        # 一般物品的品質更新邏輯
+        else:
+            item.quality = item.quality - 1
+
+        item.quality = self._clamp_quality(item.quality)
+
     def update_item_quality(self, item):
         # Sulfuras, Hand of Ragnaros 是 Special Case, 略過
         if item.name == "Sulfuras, Hand of Ragnaros":
-            return
-
-        # sell_in 會影響 quality 更新邏輯的物品的更新邏輯 (Aged Brie 和 Backstage passes to a TAFKAL80ETC concert)
-        if item.name == "Aged Brie":
-            # Aged Brie 的過期後品質更新邏輯
-            if item.sell_in < 0:
-                item.quality = item.quality + 2
-            # Aged Brie 的品質更新邏輯
-            else:
-                item.quality = item.quality + 1
+            self.update_sulfuras_quality(item)
+        elif item.name == "Aged Brie":
+            self.update_aged_brie_quality(item)
         elif item.name == "Backstage passes to a TAFKAL80ETC concert":
-            # Backstage passes to a TAFKAL80ETC concert 的品質更新邏輯
-            if item.sell_in >= 10:
-                item.quality = item.quality + 1
-            elif item.sell_in < 10 and item.sell_in >= 5:
-                item.quality = item.quality + 2
-            elif item.sell_in < 5 and item.sell_in >= 0:
-                item.quality = item.quality + 3
-            # Backstage passes to a TAFKAL80ETC concert 的過期後品質更新邏輯
-            else:
-                item.quality = item.quality - item.quality
+            self.update_backstage_pass_quality(item)
         else:
-            if item.sell_in < 0:
-                item.quality = item.quality - 2
-            # 一般物品的品質更新邏輯
-            else:
-                item.quality = item.quality - 1
-
-        # 物品品質(quality)不得超過 50
-        if item.quality > 50:
-            item.quality = 50
-        # 物品品質(quality)不得少於 0
-        if item.quality < 0:
-            item.quality = 0
+            self.udpate_normal_quality(item)
 
     def update_quality(self):
         for item in self.items:
